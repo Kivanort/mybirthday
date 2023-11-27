2f092e4acb54c78e1a8cc071b9591c74653d1005
Kivanort <152086911+Kivanort@users.noreply.github.com>
2023-11-27 21:14:32 +0300

    Add files via upload

---
+++ meScript.js
@@ -0,0 +1,10 @@
+/*var title = document.getElementById('myTitle');
+tytle.style.color = 'grey';*/
+
+var title = document.getElementById('myTitle');
+var button = document.getElementById('myButton');
+
+button.onclick = function(){
+    title.style.color = 'grey';
+}
+
